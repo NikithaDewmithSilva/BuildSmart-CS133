c90d736e4ec4d2c5215403c7f941f9de265486c5
Yusri Athas <yusriathas0786@gmail.com>
2025-02-14 07:13:53 +0530

    Home Page

--- frontendui/src/App.js
+++ frontendui/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
 
 const App = () => {
   return (
     <Router>
       <div>
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+        </Routes>
       </div>
     </Router>
   );
