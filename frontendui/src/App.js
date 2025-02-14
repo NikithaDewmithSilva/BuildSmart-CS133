993ffc45a0a4096b77ae69240179878e4e0c7ba0
Yusri Athas <yusriathas0786@gmail.com>
2025-02-14 07:36:52 +0530

    Contact Us Page

--- frontendui/src/App.js
+++ frontendui/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Aboutus from "./Components/Aboutus";
 import Services from "./Components/Services";
+import Contact from "./Components/Contact";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<Home />} /> 
           <Route path="/about" element={<Aboutus />} />
           <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
     </Router>
