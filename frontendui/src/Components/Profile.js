47752bb8f40fe6d79d872080f2c1dda9f88535b8
Yusri Athas <yusriathas0786@gmail.com>
2025-02-14 11:13:03 +0530

    Update Profile.js

--- frontendui/src/Components/Profile.js
+++ frontendui/src/Components/Profile.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./Profile.css";
 
-const Profile = () => {
+const Profile = () => {                           //Temporary
   const navigate = useNavigate();
 
   return (
