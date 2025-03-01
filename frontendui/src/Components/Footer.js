6f5d5fc7e996b613de2242dc68ba43e3a0a453fd
yesmiliyanage <yesmiranujana94@gmail.com>
2025-03-01 14:09:12 +0530

    Update Footer.js

--- frontendui/src/Components/Footer.js
+++ frontendui/src/Components/Footer.js
@@ -1,16 +1,17 @@
 import "./Footer.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram, faLinkedin, faWhatsapp, faGithub} from '@fortawesome/free-brands-svg-icons'
+import { faInstagram, faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 export default function Footer(){
     return(
         <footer className="footer">
             <div className="footer-content">
                     <ul>
-                        <li className="footer-icon"><a href="https://www.instagram.com/"><FontAwesomeIcon icon={faInstagram} /></a></li>
-                        <li className="footer-icon"><a href="https://www.facebook.com/Meta"><FontAwesomeIcon icon={faLinkedin} /></a></li>
-                        <li className="footer-icon"><a href="https://x.com/?lang=en"><FontAwesomeIcon icon={faGithub} /></a></li>
-                        <li className="footer-icon"><a href="https://web.whatsapp.com/"><FontAwesomeIcon icon={faWhatsapp} /></a></li>
+                        <li className="footer-icon"><a href="https://www.instagram.com/buildsmart.lk?igsh=MXI0bjdhN214eXR4dQ=="><FontAwesomeIcon icon={faInstagram} /></a></li>
+                        <li className="footer-icon"><a href="https://www.linkedin.com/in/build-smart-724795352/"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+                        <li className="footer-icon"><a href="https://github.com/NikithaDewmithSilva/BuildSmart-CS133"><FontAwesomeIcon icon={faGithub} /></a></li>
+                        <li className="footer-icon"><a href="https://buildsmart9.odoo.com/"><FontAwesomeIcon icon={faGlobe} /></a></li>
                     </ul>
             </div>
 
