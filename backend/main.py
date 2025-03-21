ff4b7f352ce9bfdb589648acf0e8871733c93421
SeniBiseka <senibiseka2@gmail.com>
2025-03-22 01:37:18 +0530

    Connect shareAccess in main

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from app import app as app_main
 from jsonFiles import app as app_json
 from matPlotLib import app as app_matplotlib
+from shareAccess import app as share_access
 
 # Create the main Flask app
 main_app = Flask(__name__)
@@ -14,6 +15,7 @@ CORS(main_app)
 main_app.register_blueprint(app_main)
 main_app.register_blueprint(app_json)
 main_app.register_blueprint(app_matplotlib)
+main_app.register_blueprint(share_access)
 
 if __name__ == "__main__":
     main_app.run(debug=True, host='0.0.0.0', port=5000)
