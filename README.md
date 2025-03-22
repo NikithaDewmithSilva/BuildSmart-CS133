d5fccb1f2818b4d23eca238fe715b7938b8fe0ec
NikithaDewmithSilva <nikitha.20221615@iit.ac.lk>
2025-03-23 01:48:03 +0530

    backend deployment project uploaded

--- README.md
+++
@@ -1,2 +0,0 @@
-# BuildSmart-CS133
-Implementation of our SDGP idea BuildSmart
--- frontendui/supabase.js
+++
@@ -1,7 +0,0 @@
-// src/supabase.js
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://ufernctpquewhxmrjyjr.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmZXJuY3RwcXVld2h4bXJqeWpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAyOTEwMjcsImV4cCI6MjA1NTg2NzAyN30.eL6VgQ2aXc2DR_WJbU2sHGog5x_Dlxt5LwQkzZ1VUeg';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
